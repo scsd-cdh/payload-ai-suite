bc21186c174a4ccafa7a76c92e0eb46ade8c125d
sassanghz <sassanghazi@gmail.com>
2025-05-18 13:13:33 -0400

    testing

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ import os
 import cv2
 import numpy as np
 
-def populate(X_array, y_array, path, use_nir = False, end=False):
+def populate(X_array, y_array, path, use_nir=False, end=False):
     """Populates the input arrays with preprocessed images and labels.
 
     Args:
@@ -45,19 +45,7 @@ def populate(X_array, y_array, path, use_nir = False, end=False):
         
         if end:
             for _ in range(len(X_array), len(y_array)):
-                y_array.append("N")
-
-            '''
-             modified_image = cv2.imread(image_path)
-            modified_image = cv2.resize(modified_image, (224, 224))
-            X_array.append(modified_image)
-            if not end:
-                y_array.append((image_path[0:1]))
-
-        if end:
-            for i in range(1, 99):
                 y_array.append('N')
-        '''
             
     except cv2.error as e:
         print(f"CV2 error in preprocess: {e}")
