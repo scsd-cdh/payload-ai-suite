7242399b5c54b6f85966d6880217ee428e5e6037
KP <kp-automata@protonmail.com>
2025-07-22 20:52:51 -0400

    remove comments

--- fetch.py
+++ fetch.py
@@ -609,10 +609,6 @@ def convert_sen2fire_labeled(root_dir="data/sen2fire", output_dir="data/labeled"
                 img_norm = (img_crop / img_crop.max()) * 255 # img content comes from img_norm array
                 img_norm = img_norm.astype(np.uint8) # image pixel values
 
-                # numpy_extension = '.npz'
-                # if use_nir:
-                #     numpy_extension = ".npy"
-
                 out_file = os.path.join(output_dir, final_label, f"{scene}_{fname.replace('.npz', '.png')}") # goes into the dataset based on yes or no
                 Image.fromarray(img_norm).save(out_file) # saves as a real PNG file
 
